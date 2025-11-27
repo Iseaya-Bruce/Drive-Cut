23fa70795220f668337b16c4ba85f8c9a1c56b18
Iseaya-Bruce <iseayabrucedolieveira@gmail.com>
2025-11-27 15:42:21 -0300

    Delete driving_school directory

--- driving_school/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-// Add to your schedule.php
-document.querySelectorAll('.btn-danger').forEach(btn => {
-    btn.addEventListener('click', function(e) {
-        if (!confirm('Are you sure you want to remove this availability slot?')) {
-            e.preventDefault();
-        }
-    });
-});
